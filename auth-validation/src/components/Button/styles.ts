026cd2f6bd050716ad6a2b6997fe520ebf6073fd
Guilherme Rondon <worktradestudygrra@gmail.com>
2024-09-28 14:06:36 -0300

    ts projects

---
+++ auth-validation/src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button`
+    width: 100%;
+    height: 42px;
+    background-color: #b5e48c;
+    color: #FFF;
+
+    border: 1px solid #b5e48c;
+    border-radius: 21px;
+
+    &:hover {
+        opacity: 0.6;
+        cursor:pointer;
+    }
+`
---
+++ auth-validation/src/components/Button/types.ts
@@ -0,0 +1,7 @@
+// types.ts
+export interface IButtonProps {
+    title: string;
+    onClick: () => void;
+    disabled?: boolean; // Adiciona a propriedade disabled
+  }
+  
